d8176585bc4d73265573caf87e2cb2b1aa2ef898
vikas <vikaskum5ar@gmail.com>
2024-01-30 02:51:11 +0530

    tailwind and props

---
+++ tailwindprop/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="haha kyaa re bhikmangyaa" btnText="click krooo" />
+      <Card username="miss budhi"/>
+    </>
+  )
+}
+
+export default App
